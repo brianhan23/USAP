bcbded9820972571e66b4e8269653b39a79cc11f
s3666994 <s3666994@student.rmit.edu.au>
2018-10-14 16:36:31 +1100

    Added menu looping so control is returned to previous menu

--- Ass2.sh
+++ Ass2.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
 function main_menu {
-	echo "Welcome to Led_Konfigurator!"
-	echo "============================"
-	echo "Please select an led to configure:"
-	i=0
-	for led in /sys/class/leds/*; do
+	while :
+	do
+		echo "Welcome to Led_Konfigurator!"
+		echo "============================"
+		echo "Please select an led to configure:"
+		i=0
+		for led in /sys/class/leds/*; do
+			let "i++"
+			echo "$i. $(basename "$led")"
+			opt["$i"]=$led
+		done
 		let "i++"
-		echo "$i. $(basename "$led")"
-		opt["$i"]=$led
+		echo "$i. Quit"
+		read -p "Please enter a number (1-$i) for the led to configure or quit: " input
+		case "$input" in
+			$i) exit;;
+			*) led_menu ${opt[$input]};;
+		esac
 	done
-	let "i++"
-	echo "$i. Quit"
-	read -p "Please enter a number (1-$i) for the led to configure or quit: " input
-	case "$input" in
-		$i) exit;;
-		*) led_menu ${opt[$input]};;
-	esac
 }
 
 function led_menu {
-	echo "$(basename "$1")"
-	echo "=========="
-	echo "What would you like to do with this led?"
-	echo "1) turn on"
-	echo "2) turn off"
-	echo "3) associate with a system event"
-	echo "4) associate with the performance of a process"
-	echo "5) stop association with a process' performance"
-	echo "6) quit to main menu"
-	read -p "Please enter a number (1-6) for your choice:" input
-	case "$input" in
-		1) sudo sh -c "echo 1 > $1/brightness";;
-		2) sudo sh -c "echo 0 > $1/brightness";;
-		*) echo "Please enter a number (1-6) for your choice:" ;;
-	esac 
+	while :
+	do
+		echo "$(basename "$1")"
+		echo "=========="
+		echo "What would you like to do with this led?"
+		echo "1) turn on"
+		echo "2) turn off"
+		echo "3) associate with a system event"
+		echo "4) associate with the performance of a process"
+		echo "5) stop association with a process' performance"
+		echo "6) quit to main menu"
+		read -p "Please enter a number (1-6) for your choice:" input
+		case "$input" in
+			1) sudo sh -c "echo 1 > $1/brightness";;
+			2) sudo sh -c "echo 0 > $1/brightness";;
+			6) break
+		esac 
+	done
 }
 
 function turn_led_on {
